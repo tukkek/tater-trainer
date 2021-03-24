ec598554888b4bf41cc2c01f393061b5b70647ed
Alex Henry <tukkek@gmail.com>
2021-03-24 04:36:06 -0300

    PD lock: add interact step

--- routine/pdlock.js
+++ routine/pdlock.js
@@ -1,6 +1,7 @@
 import * as routine from './routine.js'
 
-const PROMPT=document.querySelector('#pdlock .prompt')
+const INTERACT=document.querySelector('#pdlock .interact')
+const SKIP=document.querySelector('#pdlock .skip')
 const BUTTONS=document.querySelector('#pdlock .buttons')
 const SOLUTION='0131 4011 0513 4312'
 
@@ -10,18 +11,27 @@ export class PdLock extends routine.Routine{
     super('pdlock')
     this.buttons=Array.from(this.view.querySelectorAll('.buttons button'))
     this.current=0
-    this.prompt=true
+    this.interact=true
+    this.skip=false
     this.solution=Array.from(SOLUTION)
   }
   
   update(delta=false){
-    if(this.prompt){
-      PROMPT.classList.remove('hidden')
+    if(this.interact){
+      INTERACT.classList.remove('hidden')
+      SKIP.classList.add('hidden')
       BUTTONS.classList.add('hidden')
       return
     }
+    if(this.skip){
+      INTERACT.classList.add('hidden')
+      SKIP.classList.remove('hidden')
+      BUTTONS.classList.add('hidden')
+      return
+    }
+    INTERACT.classList.add('hidden')
     BUTTONS.classList.remove('hidden')
-    PROMPT.classList.add('hidden')
+    SKIP.classList.add('hidden')
     if(delta){
       this.current+=delta
       if(this.current<0) this.current=this.buttons.length-1
@@ -33,7 +43,8 @@ export class PdLock extends routine.Routine{
   setup(){
     super.setup()
     this.current=0
-    this.prompt=true
+    this.interact=true
+    this.skip=false
     this.solution=Array.from(SOLUTION)
     this.update()
   }
@@ -48,8 +59,14 @@ export class PdLock extends routine.Routine{
       this.setup()
       return
     }
-    if(this.prompt){
-      this.prompt=false
+    if(this.interact){
+      this.interact=false
+      this.skip=true
+      this.update()
+      return
+    }
+    if(this.skip){
+      this.skip=false
       this.update()
       return
     }
@@ -67,7 +84,7 @@ export class PdLock extends routine.Routine{
     this.correct()
     this.current=0
     if(this.solution[0]==' '){
-      this.prompt=true
+      this.interact=true
       this.solution.shift()
     }
     this.update()
