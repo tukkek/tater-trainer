aa03b73e8c3e19e470185aebc391d132dceb7061
Alex Henry <tukkek@gmail.com>
2021-03-24 04:25:08 -0300

    Tater keys (WASD F)

--- trainer.js
+++ trainer.js
@@ -8,9 +8,9 @@ routine.setup()
 
 window.onkeydown=(e)=>{
   let k=e.key
-  if(k=='Enter') routine.enter()
-  else if(k=='ArrowLeft'||k=='4') routine.left()
-  else if(k=='ArrowRight'||k=='6') routine.right()
-  else if(k=='ArrowUp'||k=='8') routine.up()
-  else if(k=='ArrowDown'||k=='2') routine.down()
+  if(k=='Enter'||k=='f') routine.enter()
+  else if(k=='ArrowLeft'||k=='4'||k=='a') routine.left()
+  else if(k=='ArrowRight'||k=='6'||k=='d') routine.right()
+  else if(k=='ArrowUp'||k=='8'||k=='w') routine.up()
+  else if(k=='ArrowDown'||k=='2'||k=='s') routine.down()
 }
