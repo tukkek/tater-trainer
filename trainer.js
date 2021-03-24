8ec0531b2d1402b95e44ec89f4f54737a9437bb9
Alex Henry <tukkek@gmail.com>
2021-03-24 05:09:58 -0300

    Changing numpad keys from 8462 to 5123

--- trainer.js
+++ trainer.js
@@ -9,8 +9,8 @@ routine.setup()
 window.onkeydown=(e)=>{
   let k=e.key
   if(k=='Enter'||k=='f') routine.enter()
-  else if(k=='ArrowLeft'||k=='4'||k=='a') routine.left()
-  else if(k=='ArrowRight'||k=='6'||k=='d') routine.right()
-  else if(k=='ArrowUp'||k=='8'||k=='w') routine.up()
+  else if(k=='ArrowLeft'||k=='1'||k=='a') routine.left()
+  else if(k=='ArrowRight'||k=='3'||k=='d') routine.right()
+  else if(k=='ArrowUp'||k=='5'||k=='w') routine.up()
   else if(k=='ArrowDown'||k=='2'||k=='s') routine.down()
 }
